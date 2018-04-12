4f487b10a704f7baa21a574d886f5c9f4c6ae762
kbhargavi123 <bharagvik2@gmail.com>
2018-04-12 14:44:24 -0400

    file added

---
+++ dsl-test.groovy
@@ -0,0 +1,11 @@
+job('$job_name') {
+    scm {
+        git('git://github.com/quidryan/aws-sdk-test.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
