4d23e8492911f1af8af47eb46fda237ca2a1f3fe
kbhargavi123 <bharagvik2@gmail.com>
2018-04-16 15:26:59 -0400

    Update desl_test1.groovy

--- desl_test1.groovy
+++ desl_test1.groovy
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:desl-test1.groovy
-def jobname="${jobname}"
-
-=======
-def jobname ="${jobName}_sh"
->>>>>>> 0f5bbb9b8f2080cf923064c8338a58e27d40a13f:desl_test1.groovy
+def jobname ="${jobname}"
 job(jobname) {
     scm {
         git('https://github.com/kbhargavi123/antprojects.git')
@@ -11,10 +6,9 @@ job(jobname) {
     triggers {
         scm('H/15 * * * *')
     }
-	
+}
     steps {
-	     
-<<<<<<< HEAD:desl-test1.groovy
+	  
        ant('jar')
        {
 	    antInstallation '1.9.6'
@@ -22,8 +16,3 @@ job(jobname) {
 	   
        }
 }
-=======
-       shell('pwd')
-    }
-}
->>>>>>> 0f5bbb9b8f2080cf923064c8338a58e27d40a13f:desl_test1.groovy
