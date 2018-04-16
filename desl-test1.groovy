ba04b7b5aaf3cffe9638b50f26037044e1c033bc
kbhargavi123 <bharagvik2@gmail.com>
2018-04-16 15:11:31 -0400

    existing file changed

--- desl-test1.groovy
+++ desl-test1.groovy
@@ -1,12 +1,19 @@
-job(job_name) {
+def jobname="${jobname}"
+
+job(jobname) {
     scm {
-        git('git://github.com/quidryan/aws-sdk-test.git')
+        git('https://github.com/kbhargavi123/antprojects.git')
     }
     triggers {
         scm('H/15 * * * *')
     }
+	
     steps {
 	     
-       sh 'pwd'
-    }
+       ant('jar')
+       {
+	    antInstallation '1.9.6'
+		buildFIle "build.xml"
+	   
+       }
 }
